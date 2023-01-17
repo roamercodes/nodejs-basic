27e446697e638a6c62124bb3ddcb2ff1d912da14
roamercodes <mkholilullah29@gmail.com>
2023-01-18 02:38:17 +0700

    modularization

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-const server = new Server({
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : 'dicoding.com',
-});
+// const coffee = require('./lib/coffee');
+ 
+// console.log(coffee);
+ 
+/**
+ * node app.js
+ *
+ * output:
+ * { name: 'Tubruk', price: 15000 }
+ */
+
+/**
+* Gunakan object destructuring untuk mengimpor lebih dari satu nilai pada modul.
+*/
+const { firstName, lastName } = require('./user');
+ 
+ 
+console.log(firstName);
+console.log(lastName);
+ 
+ 
+/**
+* output:
+* Harry
+* Potter
+*/
