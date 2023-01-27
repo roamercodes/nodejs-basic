2b2015529e666a0050a0a1559e1eebac32b414de
roamercodes <mkholilullah29@gmail.com>
2023-01-27 22:11:17 +0700

    routing

--- routing/server.js
+++ routing/server.js
@@ -1,26 +1,6 @@
-const http = require('http');
-
-// const requestListener = (request, response) => {
-//     response.setHeader('Content-type', 'text/html');
-
-//     response.statusCOde = 200;
-//     response.end('<h1>Halo HTTP Server!</h1>');
-// };
-
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
     response.statusCode = 200;
-
-const server = http.createServer(requestListener);
-
-const port = 5000;
-const host = 'localhost';
-
-server.listen(port, host, () => {
-    console.log(`Server berjalan pada http://${host}:${port}`);
-});
-
-
 
     // destructuring object to get property of url
     const { method, url } = request;
